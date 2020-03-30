f49cad0d76377bd3f00cb2a30cf957e6275c6324
Zain Siddiqui <zainsiddiqui@Zains-MacBook-Pro.local>
2020-03-29 19:43:10 -0700

    final

--- bot.py
+++ bot.py
@@ -1,15 +1,9 @@
 from flask import Flask, request
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
-
 import csv
-import requests
-
 import urllib.request
-
 from collections import defaultdict
-
-#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=demo&datatype=csv'
 
 url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
 filename = 'us-states.csv'
@@ -18,23 +12,16 @@ urllib.request.urlretrieve(url, filename)
 dd = {}
 state_list = []
 
+# Build dictionary from url
 with open(filename) as csv_file:
 
     state_tracker = 0
     for row in csv_file:
-
         my_list = row.split(",")
-        print(my_list)
         if my_list[1] not in dd.keys():
             dd[my_list[1] ] = 5;
-
-
         if my_list[1] in dd.keys():
             dd[ my_list[1] ] = my_list[3]
-        
-    print ("Resultant dictionary", str(dd) ) 
-
-
 
 
 app = Flask(__name__)
@@ -46,19 +33,6 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    # if 'quote' in incoming_msg:
-    #     # return a quote
-    #     r = requests.get('https://api.quotable.io/random')
-    #     if r.status_code == 200:
-    #         data = r.json()
-    #         quote = f'{data["content"]} ({data["author"]})'
-    #     else:
-    #         quote = 'I could not retrieve a quote at this time, sorry.'
-    #     msg.body(quote)
-    #     responded = True
-    # if 'cat' in incoming_msg:
-    #     # return a cat pic
-    #     msg.media('https://cataas.com/cat')
 
     if incoming_msg:
         responded = True
@@ -68,20 +42,6 @@ def bot():
         else:
             print(str(incoming_msg))
             msg.body("Enter a valid state name.")
-
-
-    
-
-
-
-
-
-    # if 'mudbone' in incoming_msg:
-    #     msg.media('https://pbs.twimg.com/profile_images/726295664032772098/fe1hGAKb_400x400.jpg')
-    #     responded = True
-
-    # if not responded:
-    #     msg.body('I only know about famous quotes and cats, sorry!')
 
     return str(resp)
 
